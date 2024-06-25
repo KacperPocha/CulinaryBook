1deba6015a4c756b0ee8bdd4bfe6f4c0b7707046
Kacper Pocha <kacper.pocha123@gmail.com>
2024-06-25 20:55:46 +0200

    Dodaj pliki projektów.

---
+++ CulinaryBook/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulinaryBook.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Directions { get; set; }
+        public string Category { get; set; }
+        public string Taste { get; set; }
+        public string Time { get; set; }
+        public DateTime CreatedData { get; set; }
+        public DateTime UpdatedData { get; set;}
+    }
+}
---
+++ CulinaryBook/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace CulinaryBook.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
